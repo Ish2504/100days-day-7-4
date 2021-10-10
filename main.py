5140e83d8379f7b18977bd696a5b5da3a86a640e
Ish2504 <87877194+Ish2504@users.noreply.github.com>
2021-10-10 20:14:30 +0000

    done

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ word_length = len(chosen_word)
 
 #TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
 #Set 'lives' to equal 6.
-
+lives = 6
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -88,7 +88,9 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
-
+    if guess not in chosen_word:
+      lives -= 1
+      print(stages[lives])
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -96,5 +98,7 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
+    elif lives == 0:
+        end_of_game = True
+        print("You lose.")
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
